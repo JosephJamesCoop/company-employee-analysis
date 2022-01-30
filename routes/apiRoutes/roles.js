f72fb93796eea1c2fd8c562b6a99618372e14523
JosephJamesCoop <cooper_3869@live.com>
2022-01-30 10:42:51 -0800

    updated View all prompts

--- routes/apiRoutes/roles.js
+++ routes/apiRoutes/roles.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
-
+const inquirer = require("inquirer");
+const { application } = require('express');
 // const repeat = require('application()')
 
 
@@ -10,23 +11,48 @@ const db = require('../../db/connection');
 const DB = {
   ViewAllDepartments() {
     console.log('View All Departments');
+    const sql = `SELECT * FROM departments`; 
+    db.query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+    });
   },
 
   ViewAllRoles() {
     console.log('View All Roles');
+    const sql = `SELECT * FROM role`; 
+    db.query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+    });
   },
+
   ViewAllEmployees() {
     console.log('View All Employees');
+    const sql = `SELECT * FROM employee`; 
+    db.query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+    });
   },
+
   AddADepartment() {
     console.log('Add A Departments');
 },
+
   AddARole() {
     console.log('Add A Role');
   },
+
+  AddAnEmployee() {
+    console.log('Add An Employee');
+  },
+
   UpdateAnEmployeeRole() {
     console.log('Update An Employee Role');
-  }, Quit() {
+  }, 
+  
+  Quit() {
     console.log('You selected Quit');
   },
 };
