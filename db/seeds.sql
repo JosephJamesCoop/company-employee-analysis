7c832675c46fd7c408f7ebe29fd0f29d90c96ea9
JosephJamesCoop <cooper_3869@live.com>
2022-02-05 18:35:48 -0800

    finalized prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,27 +7,27 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Employee', '25001.00', 1),
-  ('Manager', '45001.00', 1),
-  ('Employee', '25002.00', 2),
-  ('Manager', '45002.00', 2),
-  ('Employee', '25003.00', 3),
-  ('Manager', '45003.00', 3),
-  ('Employee', '25004.00', 4),
-  ('Manager', '45004.00', 4);
+  ('Employee', '25000.00', 1),
+  ('Manager', '45000.00', 1),
+  ('Employee', '25000.00', 2),
+  ('Manager', '45000.00', 2),
+  ('Employee', '25000.00', 3),
+  ('Manager', '45000.00', 3),
+  ('Employee', '25000.00', 4),
+  ('Manager', '45000.00', 4);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Jack', 'clothingManager', '1', '0'),
-  ('John', 'clothingEmployee1', '2', '1'),
-  ('Joe', 'furnitureManager', '3', '0'),
-  ('Josh', 'furnitureEmployee1', '4', '3'),
-  ('Jake', 'outdoorsManager', '5', '0'),
-  ('Jill', 'outdoorsEmployee1', '6', '5'),
-  ('Jenny', 'registerManager', '7', '0'),
-  ('Janet', 'registerEmployee1', '8', '7'),
-  ('Jacob', 'clothingEmployee2', '2', '1'),
-  ('Juan', 'furnitureEmployee2', '4', '3'),
-  ('Joanne', 'outdoorsEmployee2', '6', '5'),
-  ('Jordan', 'registerEmployee2', '8', '7');
+  ('Jack', 'Johnson', '1', '0'),
+  ('John', 'Jones', '2', '1'),
+  ('Joe', 'Jackson', '3', '0'),
+  ('Josh', 'James', '4', '3'),
+  ('Jake', 'Jenkins', '5', '0'),
+  ('Jill', 'Johnston', '6', '5'),
+  ('Jenny', 'Jordan', '7', '0'),
+  ('Janet', 'Jacobs', '8', '7'),
+  ('Jacob', 'Jensen', '2', '1'),
+  ('Juan', 'Jacinto', '4', '3'),
+  ('Joanne', 'Jacobson', '6', '5'),
+  ('Jordan', 'Jennings', '8', '7');
--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require('mysql2');
-// const DB = require("./routes/apiRoutes/roles.js");
-// const db = require('./db/connection');
 
-
-// const db = require('./db/connection');
-// const routes = require('./routes/roles.js');
-
-// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -26,18 +19,13 @@ const db = mysql.createConnection(
   },
   console.log('Connected to the employees database.')
 );
-// // use API routes
-// app.use('/api', routes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-
-
-
-//one file
+// main question
 const userPrompts = {
   promptHomeMenu() {
     return inquirer.prompt([{
@@ -49,128 +37,186 @@ const userPrompts = {
   }
 };
 
-
-
 const DB = {
   ViewAllDepartments() {
     console.log('View All Departments');
-    const sql = `SELECT * FROM departments`; 
+    const sql = `SELECT * FROM departments`;
     db.query(sql, (err, rows) => {
       if (err) throw err;
       console.table(rows);
-    })
-    application();
+      application();
+    });
+    
   },
 
   ViewAllRoles() {
     console.log('View All Roles');
-    const sql = `SELECT * FROM role`; 
+    const sql = `SELECT * FROM role`;
     db.query(sql, (err, rows) => {
       if (err) throw err;
       console.table(rows);
+      application();
     });
-    application();
   },
 
   ViewAllEmployees() {
     console.log('View All Employees');
-    const sql = `SELECT * FROM employee`; 
+    const sql = `SELECT * FROM employee`;
     db.query(sql, (err, rows) => {
       if (err) throw err;
       console.table(rows);
+      application();
     });
-    application();
   },
-  
+
   AddADepartment() {
     console.log('Add A Departments');
-  inquirer.prompt([
+    inquirer.prompt([
       {
-      type: 'input',
-      name: 'name',
-      message: 'Please select a Name for the new Department: ',
-    }
-  ]).then(
-  department => {
-const name = department.name;
-    const sql = `INSERT INTO departments (department)
+        type: 'input',
+        name: 'name',
+        message: 'Please type a name for the new Department: ',
+      }
+    ]).then(
+      department => {
+        const name = department.name;
+        const sql = `INSERT INTO departments (department)
   VALUES (?)`;
-  const params = [name];
-  db.query(sql, params, (err, rows) => {
-    if (err) throw err;
-    console.log(`Added ${name} to Departments`);
-  }) 
-  DB.ViewAllDepartments();
-}) 
-},
+        const params = [name];
+        db.query(sql, params, (err, rows) => {
+          if (err) throw err;
+          console.log(`Added ${name} to Departments`);
+        })
+        DB.ViewAllDepartments();
+      })
+  },
 
   AddARole() {
     console.log('Add A Role');
     inquirer.prompt([
       {
-      type: 'input',
-      name: 'title',
-      message: 'Please select a title for the new role: ',
-    },
-    {
-      type: 'input',
-      name: 'salary',
-      message: 'Please select a salary for the new role: (no $ or , ) ',
-    },
-    {
-      type: 'input',
-      name: 'deptId',
-      message: 'Please select a department ID for the new role: ',
-    },
-  ]).then(
-  role => {
-const title = role.title;
-const salary = role.salary;
-const deptId = role.deptId
-    const sql = `INSERT INTO role (title, salary, department_id)
+        type: 'input',
+        name: 'title',
+        message: 'Please type a title for the new role: ',
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'Please type a salary for the new role: (no $ or , ) ',
+      },
+      {
+        type: 'input',
+        name: 'deptId',
+        message: 'Please type a department ID for the new role: ',
+      },
+    ]).then(
+      role => {
+        const title = role.title;
+        const salary = role.salary;
+        const deptId = role.deptId
+        const sql = `INSERT INTO role (title, salary, department_id)
   VALUES (?, ?, ?)`;
-  const params = [title, salary, deptId];
-  db.query(sql, params, (err, rows) => {
-    if (err) throw err;
-    console.log(`Added ${title, salary, deptId} to Role`);
-  }) 
-  DB.ViewAllRoles();
-}) 
+        const params = [title, salary, deptId];
+        db.query(sql, params, (err, rows) => {
+          if (err) throw err;
+          console.log(`Added ${title, salary, deptId} to Role`);
+        })
+        DB.ViewAllRoles();
+      })
   },
   AddAnEmployee() {
     console.log('Add An Employee');
-
-
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-    VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-    db.query(sql, params, (err, rows) => {
-      if (err) throw err;
-      console.table(rows);
-    });
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'fName',
+        message: "Please type the new employee's First Name:",
+      },
+      {
+        type: 'input',
+        name: 'lName',
+        message: "Please type the new employee's Last Name:",
+      },
+      {
+        type: 'input',
+        name: 'roleId',
+        message: 'Please type a department ID for the new employee: ',
+      },
+      {
+        type: 'input',
+        name: 'manId',
+        message: 'Please type a manager ID for the new employee: ',
+      },
+    ]).then(
+      employee => {
+        const fName = employee.fName;
+        const lName = employee.lName;
+        const deptId = employee.roleId
+        const manId = employee.manId
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+  VALUES (?, ?, ?, ?)`;
+        const params = [fName, lName, deptId, manId];
+        db.query(sql, params, (err, rows) => {
+          if (err) throw err;
+          console.log(`Added ${fName, lName, deptId, manId} to Role`);
+        })
+        DB.ViewAllEmployees();
+      })
   },
   UpdateAnEmployeeRole() {
     console.log('Update An Employee Role');
 
+    console.log('Modify an Employee');
+    const sql = `SELECT * FROM employee`;
+    const employeeList = [];
+    db.query(sql, (err, rows) => {
+      if (err) throw err;
+      for (let i = 0; i < rows.length; i++) {
+        employeeList.push(JSON.stringify(rows[i]))
+      }
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'employeeChoice',
+          message: 'Who would you like to update?',
+          choices: employeeList,
+        },
+        {
+          type: 'input',
+          name: 'roleId',
+          message: 'Please type a department ID for the new employee: ',
+        },
+        {
+          type: 'input',
+          name: 'manId',
+          message: 'Please type a manager ID for the new employee: ',
+        },
+      ]).then(
+        employee => {
+          const splitted = employee.employeeChoice.split('"');
+          const partOne = splitted[2];
+          const partTwo = partOne.split(':');
+          const idNum = partTwo[1].replace(',', '');
+          console.log("test", idNum);
+          const fName = splitted[5];
+          const lName = splitted[9];
+          const deptId = employee.roleId
+          const manId = employee.manId
+          const sql = `UPDATE employee SET role_id = ${deptId}, manager_id = ${manId} WHERE id = ${idNum};`;
 
-    
-  }, Quit() {
-    console.log('You selected Quit');
+          db.query(sql, (err, rows) => {
+            if (err) throw err;
+            console.log(`Updated role-id to ${deptId} and manager_id to ${manId} for ${fName}, ${lName} `);
+          })
+          DB.ViewAllEmployees();
+        })
+    })
   },
 };
 
-
-
-
-
-
-
-
-
-// third file
 async function application() {
   const { menuChoice } = await userPrompts.promptHomeMenu();
-  if (menuChoice === 'Quit') return console.log('Goodbye!');
+  if (menuChoice === 'Quit') return process.exit(console.log('Goodbye!'));
   DB[menuChoice.split(' ').join('')]();
 };
 
